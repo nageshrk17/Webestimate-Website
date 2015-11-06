b08cdd281f20dd395898059429d5b9f82a568f34
nageshk <nagesh.rk17@gmail.com>
2015-11-06 09:27:38 +0530

    Webestimate

---
+++ js/script.js
@@ -0,0 +1,34 @@
+/* Function for ScrollUp */
+
+$('.smoothscroll').on('click', function(e) {
+    e.preventDefault();
+
+    var target = this.hash,
+        $target = $(target);
+
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+    }, 800, 'swing', function() {
+        window.location.hash = target;
+    });
+});
+
+
+// Function to Select only One Checkbox.
+
+var previousCheckId;
+
+function toggle(chkBox) {
+    if (chkBox.checked) {
+        if (previousCheckId) {
+            document.getElementById(previousCheckId).checked = false;
+        }
+        previousCheckId = chkBox.getAttribute('id');
+    }
+}
+
+// Function to Upload Files.
+
+$("#upload").click(function() {
+    $("#hide").click();
+});
